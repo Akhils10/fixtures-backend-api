c2e4c4f2b7f4813faa34489e8d760bda5f0a693c
Philip Akhils <philipakhilome@gmail.com>
2020-01-28 14:55:35 +0100

    Remove views

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://user:pass@postgres:5432/db'
+    connection: 'postgres://user:pass@192.168.99.100:35432/db'
   }
 
 };
