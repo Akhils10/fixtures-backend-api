ddd4862af13cf4c28745bf1cfa9d1dcd82cae0d1
Philip Akhils <philipakhilome@gmail.com>
2020-01-31 01:17:56 +0100

    fixed redis connection on heroku

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ const logger = require('morgan');
 const session = require('express-session');
 const redisStore = require('connect-redis')(session);
 const redis = require('redis');
-let client;
-
-if(process.env.NODE_ENV === 'production'){
-    client = redis.createClient('redis://h:pcc7f42fcb7baff28a020b60bc2def303b665db84586f2f8226da890651d203fb@ec2-3-210-246-86.compute-1.amazonaws.com:10749');
-}else{
-    client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
-}
+const client = redis.createClient('redis://h:pcc7f42fcb7baff28a020b60bc2def303b665db84586f2f8226da890651d203fb@ec2-3-210-246-86.compute-1.amazonaws.com:10749');
 
 client.on('connect', function(){
     console.log('connected to redis');
