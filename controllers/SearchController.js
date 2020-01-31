06dbd0679f6f89f49e935505e7dae2ab7eda665f
Philip Akhils <philipakhilome@gmail.com>
2020-01-31 03:07:32 +0100

    Fixed issue with search not working

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -4,18 +4,10 @@ const Fixtures = require('../models/Fixtures');
 exports.search = async (req, res) => {
     let searchNeedle = req.params.title
     let searchData = [];
-    let response = []
+    
+    await Teams.getTeams().then(teams => searchData.push({type: "teams", data: teams.filter(data => data.name.toLowerCase() == searchNeedle.toLowerCase())}))
+    await Fixtures.getFixtures().then(fixtures => searchData.push({type: "fixtures", data: fixtures.filter(data => data.home_team.toLowerCase() == searchNeedle.toLowerCase() || data.away_team.toLowerCase() == searchNeedle.toLowerCase())}))
 
-    await Teams.getTeams().then(teams => searchData.push({type: "teams", data: teams}))
-    await Fixtures.getFixtures().then(fixtures => searchData.push({type: "fixtures", data: fixtures}))
-    response = searchData.filter(data => {
-        if(data.type === "teams"){
-            return data.data[0].name.toLowerCase() == searchNeedle.toLowerCase()
-        }else if(data.type === "fixtures"){
-            return data.data[0].home_team.toLowerCase() == searchNeedle.toLowerCase() || data.data[0].away_team.toLowerCase() == searchNeedle.toLowerCase()
-        }
-    });
-
-    res.status(200).json(response);
+    res.status(200).json(searchData);
 
 }
